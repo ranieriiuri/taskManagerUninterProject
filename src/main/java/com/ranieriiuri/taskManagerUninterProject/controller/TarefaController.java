dd8fa4eeca1943129192e0048129562b256faf03
ranieriiuri <iuriranierioliveira@gmail.com>
2025-08-30 14:31:00 -0300

    Edição de tarefa funcionando

--- src/main/java/com/ranieriiuri/taskManagerUninterProject/controller/TarefaController.java
+++ src/main/java/com/ranieriiuri/taskManagerUninterProject/controller/TarefaController.java
@@ -1,7 +1,10 @@
 package com.ranieriiuri.taskManagerUninterProject.controller;
 
 import com.ranieriiuri.taskManagerUninterProject.entity.Tarefa;
+import com.ranieriiuri.taskManagerUninterProject.service.TarefaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +18,9 @@ import java.util.List;
 @RequestMapping("/tarefas")
 public class TarefaController {
 
+    @Autowired
+    private TarefaService service;
+
     List<Tarefa> tarefas = new ArrayList<>();
 
     @GetMapping("/cadastro")
@@ -24,74 +30,36 @@ public class TarefaController {
 
     @PostMapping("/salvar")
     public String salvar(Tarefa tarefa) {
-        Long id = tarefas.size() + 1L;
-        Tarefa t = new Tarefa();
-
-        t.setId(id);
-        t.setNome(tarefa.getNome());
-        t.setDataEntrega(tarefa.getDataEntrega());
-        t.setResponsavel(tarefa.getResponsavel());
-
-        tarefas.add(t);
+        service.salvar(tarefa);
 
         return "redirect:/tarefas/lista";
     }
 
     @GetMapping("/lista")
-    public ModelAndView lista () {
-        ModelAndView mv = new ModelAndView("/tarefa/lista");
-        mv.addObject("tarefas", tarefas);
-
-        return mv;
+    public String lista(ModelMap model) {
+        model.addAttribute("tarefas", service.buscaTodos());
+        return "tarefa/lista";
     }
 
     @PostMapping("/editar")
     public String editar(Tarefa tarefa) {
 
-        Tarefa t = new Tarefa();
-        for (int i = 0; i < tarefas.size(); i++) {
-            if (tarefas.get(i).getId().equals(tarefa.getId())) {
-                t = tarefas.get(i);
-
-            }
-        }
-
-        tarefas.set(tarefas.indexOf(t), tarefa);
-
-
-        return "redirect:/tarefas/lista";
+        service.editar(tarefa);
+        return "redirect:/tarefas/cadastro";
     }
 
     @GetMapping("/excluir/{id}")
-    public String excluir(@PathVariable("id") Long id ) {
+    public String excluir(@PathVariable("id") Long id, ModelMap model ) {
 
-        Tarefa tarefa;
-        for (int i = 0; i < tarefas.size(); i++) {
-            if (tarefas.get(i).getId().equals(id)) {
-                tarefa = tarefas.get(i);
-                tarefas.remove(tarefa);
-
-            }
-        }
-
-        return "redirect:/tarefas/lista";
+        service.excluir(id);
+        return lista(model);
     }
 
     @GetMapping("/editar/{id}")
-    public ModelAndView preEditar(@PathVariable("id") Long id) {
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("tarefa/cadastro");
+    public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+        model.addAttribute("tarefa", service.buscarPorId(id));
 
-        Tarefa tarefa = null;
-        for (int i = 0; i < tarefas.size(); i++) {
-            if (tarefas.get(i).getId().equals(id)) {
-                tarefa = tarefas.get(i);
-
-            }
-        }
-
-        mv.addObject("tarefa", tarefa);
-        return mv;
+        return "/tarefa/cadastro";
     }
 
 }
