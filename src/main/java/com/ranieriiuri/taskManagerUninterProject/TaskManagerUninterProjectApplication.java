eb491e5aed53709604aa7104a88a3c066d984e3f
ranieriiuri <iuriranierioliveira@gmail.com>
2025-08-30 10:27:37 -0300

    first commit

---
+++ src/main/java/com/ranieriiuri/taskManagerUninterProject/TaskManagerUninterProjectApplication.java
@@ -0,0 +1,14 @@
+package com.ranieriiuri.taskManagerUninterProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerUninterProjectApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(TaskManagerUninterProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ranieriiuri/taskManagerUninterProject/TaskManagerUninterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranieriiuri.taskManagerUninterProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerUninterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
