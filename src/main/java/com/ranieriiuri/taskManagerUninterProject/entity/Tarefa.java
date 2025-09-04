b9865661c60ebce3a1240a32ef7be2414aad838f
ranieriiuri <iuriranierioliveira@gmail.com>
2025-09-04 00:11:12 -0300

    Testes da Tarefa

--- src/main/java/com/ranieriiuri/taskManagerUninterProject/entity/Tarefa.java
+++ src/main/java/com/ranieriiuri/taskManagerUninterProject/entity/Tarefa.java
@@ -6,15 +6,21 @@ import java.util.Objects;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name="TAREFA")
 public class Tarefa extends AbstractEntity<Long> {
 
+    @NotBlank(message = "O nome da tarefa é obrigatório.")
+    @Size(min = 2, max = 60, message = "O nome deve ter no mínimo {min} caracteres.")
     @Column(name = "nome", nullable = false, unique = true, length = 60)
     private String nome;
 
+    @NotNull(message = "O campo de data é obrigatório")
     @Column(name = "data_entrega", nullable = false, columnDefinition = "DATE")
     @DateTimeFormat(iso= DateTimeFormat.ISO.DATE)
     private LocalDate dataEntrega;
---
+++ src/test/java/com/ranieriiuri/taskManagerUninterProject/service/TarefaServiceTest.java
@@ -0,0 +1,65 @@
+package com.ranieriiuri.taskManagerUninterProject.service;
+
+import com.ranieriiuri.taskManagerUninterProject.entity.Tarefa;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class TarefaServiceTest {
+
+    @Autowired
+    private TarefaServiceImpl ts;
+
+    @Test
+    @Order(1)
+    void salvar() {
+        Tarefa tarefa = new Tarefa();
+
+        tarefa.setNome("Revisao");
+        tarefa.setDataEntrega(LocalDate.of(2025, 12, 25));
+        tarefa.setResponsavel("Ranieri");
+
+        ts.salvar(tarefa);
+    }
+
+    @Test
+    void buscaTodos() {
+    }
+
+    @Test
+    @Order(2)
+    public void buscarPorId() {
+        Tarefa tarefa = ts.buscarPorId(2L);
+        System.out.println(tarefa);
+    }
+
+    @Test
+    @Order(3)
+    public void editar() {
+        Tarefa tarefa = ts.buscarPorId(2L);
+        tarefa.setNome("Review");
+        ts.editar(tarefa);
+    }
+    @Test
+    @Order(4)
+    public void excluir() {
+        Tarefa tarefa = ts.buscarPorId(2L);
+
+        ts.excluir(tarefa.getId());
+
+
+    }
+}
